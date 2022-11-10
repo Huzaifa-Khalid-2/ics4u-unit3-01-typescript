ec2b88bde143c7af42415305faa7855b5bd9efb6
Huzaifa-Khalid-2 <Huzaifa-Khalid-2@users.noreply.github.com>
2022-11-10 14:55:54 +0000

    Prettified Code!

--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,26 @@
  */
 /**
  * The function gets the reversestring
- * 
+ *
  * @param {string} string - string value
  * @returns {string} - Retrub value
  */
 
 function reverseString(string: string): string {
-   if (string === '') {
-        return string
- } else {
+  if (string === '') {
+    return string
+  } else {
     return reverseString(string.slice(1, 10)) + string[0]
- }
+  }
 }
 
- function main() {
-  const aString = "recursion"
+function main() {
+  const aString = 'recursion'
 
- console.log("")
- console.log(`The orignal string is: ${aString}`)
- console.log(`The reversed string is: ${reverseString(aString)}`)
- console.log('\nDone')
- }
+  console.log('')
+  console.log(`The orignal string is: ${aString}`)
+  console.log(`The reversed string is: ${reverseString(aString)}`)
+  console.log('\nDone')
+}
 
 main()
